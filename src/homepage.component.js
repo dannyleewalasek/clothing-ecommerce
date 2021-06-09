4fc809648c85e523fbb2b65da316f98bdf16825d
dannyleewalasek <dannyleewalasek@gmail.com>
2021-06-09 14:43:15 +0100

    initial commit

---
+++ src/homepage.component.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title"> hats </h1>
+            <span className="subtitle"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
