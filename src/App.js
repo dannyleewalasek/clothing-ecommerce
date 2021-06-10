3b9e9d217095f1099e43bfe3c0d66bd75dcd4e80
dannyleewalasek <dannyleewalasek@gmail.com>
2021-06-10 16:17:10 +0100

    added collection and collection items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component.jsx";
+
+const HatsPage = () => <div>hats page</div>;
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* //exact is needed here as home would also render at /hats as it contains
+      //'/' //without exact //with switch as soon as one route matches the path,
+      //it only renders that route, switch is useful as it gives more control,
+      // makes sure only 1 route is rendered, so no accidents */}
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./preview-collection.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
+
+const PreviewCollection = ({ title, items }) => (
+  <div className="preview-collection">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps}></CollectionItem>
+        ))}
+    </div>
+  </div>
+);
+
+export default PreviewCollection;
