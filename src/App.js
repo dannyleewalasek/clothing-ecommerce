67ffc51c98c2e6cf04ea29abc5e200db1db119fa
dannyleewalasek <dannyleewalasek@gmail.com>
2021-06-10 16:29:10 +0100

    added header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component";
 
 const HatsPage = () => <div>hats page</div>;
 
 function App() {
   return (
     <div>
+      <Header></Header>
       {/* //exact is needed here as home would also render at /hats as it contains
       //'/' //without exact //with switch as soon as one route matches the path,
       //it only renders that route, switch is useful as it gives more control,
