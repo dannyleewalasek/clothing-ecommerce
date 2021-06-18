ba1aef0d436fa2ad32ca724c72123f7875b779b2
dannyleewalasek <dannyleewalasek@gmail.com>
2021-06-18 11:37:49 +0100

    implementing cart and cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
@@ -8,7 +11,7 @@ import "./header.styles.scss";
 
 import { auth } from "../../firebase/firebase.utils";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo"></Logo>
@@ -30,12 +33,16 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon></CartIcon>
+      {hidden ? null : <CartDropDown></CartDropDown>}
     </div>
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+//destructuring nested, want the current value off the user which is off the styate
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
 import { combineReducers } from "redux";
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
